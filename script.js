18379f68866c0ecaba682813a3f54bbf0cd11e4d
juliagoedert22 <164075119+juliagoedert22@users.noreply.github.com>
2025-08-25 08:15:00 -0300

    .

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 const caixaPrincipal = document.querySelector('.caixa-principal');
-
 const caixaPerguntas = document.querySelector('.caixa-perguntas');
-
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
-
 const caixaResultado = document.querySelector('.caixa-resultado');
-
 const textoResultado = document.querySelector('.texto-Resultado');
-
-const lista = [];
-
 const perguntas = [
     {
         enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
